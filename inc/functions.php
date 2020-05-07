fd6ffea2a3bad8a391d7044fc792cb4ca7dfe112
Njengah <joe@njengah.com>
2020-05-07 18:42:02 +0300

    readme updated

--- inc/functions.php
+++ inc/functions.php
@@ -1,5 +1,4 @@
 <?php 
-
 /**
  * Enqueue both scripts and styles.
  */
--- wocommerce-send-to-mpesa-payment.php
+++ wocommerce-send-to-mpesa-payment.php
@@ -21,164 +21,195 @@ if (!defined('ABSPATH')) {
  *
  */
 add_action('plugins_loaded', 'init_send_to_mpesa_gateway_class');  
+
 function init_send_to_mpesa_gateway_class()
 {
 
-    // Move this class to the admin 
-    class WC_Gateway_Send_To_Mpesa extends WC_Payment_Gateway
-    {
-
-        public $domain;
+    if (class_exists('WC_Payment_Gateway')) {
 
         /**
-         * Constructor for the gateway.
+         * WooCommerce Send to Mpesa Payment Gateway Main Class 
          */
-        public function __construct()
+        class WC_Gateway_Send_To_Mpesa extends WC_Payment_Gateway
         {
 
-            $this->domain = 'send-to-mpesa-payment';
+            public $domain;
 
-            $this->id                 = 'send_to_mpesa_';
-            $this->icon               = apply_filters('woocommerce_send_to_mpesa_gateway_icon', '');
-            $this->has_fields         = false;
-            $this->method_title       = __('Send to Mpesa', $this->domain);
-            $this->method_description = __('Allows customers to send payments to Mpesa phone number Example 0722 XXX XXX.', $this->domain);
+            /**
+             * Constructor for the gateway.
+             */
+            public function __construct()
+            {
 
-            // Load the settings.
-            $this->init_form_fields();
-            $this->init_settings();
+                $this->domain = 'send-to-mpesa-payment';
 
-            // Define user set variables
-            $this->title        = $this->get_option('title');
-            $this->description  = $this->get_option('description');
-            $this->mpesa_name   = $this->get_option('mpesa_name');
-            $this->instructions = $this->get_option('instructions', $this->description);
-            $this->order_status = $this->get_option('order_status', 'completed');
+                $this->id                 = 'send_to_mpesa_';
+                $this->icon               = apply_filters('woocommerce_send_to_mpesa_gateway_icon', '');
+                $this->has_fields         = false;
+                $this->method_title       = __('Send to Mpesa', $this->domain);
+                $this->method_description = __('Allows customers to send payments to Mpesa phone number Example 0722 XXX XXX.', $this->domain);
 
-            // Actions
-            add_action('woocommerce_update_options_payment_gateways_' . $this->id, array($this, 'process_admin_options'));
-            add_action('woocommerce_thankyou_send_to_mpesa_', array($this, 'thankyou_page'));
+                // Load the settings.
+                $this->init_form_fields();
+                $this->init_settings();
 
-            // Customer Emails
-            add_action('woocommerce_email_before_order_table', array($this, 'email_instructions'), 10, 3);
-        }
+                // Define user set variables
+                $this->title        = $this->get_option('title');
+                $this->description  = $this->get_option('description');
+                $this->mpesa_name   = $this->get_option('mpesa_name');
+                $this->instructions = $this->get_option('instructions', $this->description);
+                $this->order_status = $this->get_option('order_status', 'completed');
 
-        /**
-         * Initialise Gateway Settings Form Fields.
-         */
-        public function init_form_fields()
-        {
+                // Actions
+                add_action('woocommerce_update_options_payment_gateways_' . $this->id, array($this, 'process_admin_options'));
+                add_action('woocommerce_thankyou_send_to_mpesa_', array($this, 'thankyou_page'));
 
-            $this->form_fields = array(
-                'enabled' => array(
-                    'title'   => __('Enable or Disable', $this->domain),
-                    'type'    => 'checkbox',
-                    'label'   => __('Enable Send Mpesa Payment', $this->domain),
-                    'default' => 'yes'
-                ),
-                'title' => array(
-                    'title'       => __('Payment Title', $this->domain),
-                    'type'        => 'text',
-                    'description' => __('This controls the title which the user sees during checkout.', $this->domain),
-                    'default'     => __('Send to Mpesa', $this->domain),
-                    'desc_tip'    => true,
-                ),
-                'order_status' => array(
-                    'title'       => __('Order Status', $this->domain),
-                    'type'        => 'select',
-                    'class'       => 'wc-enhanced-select',
-                    'description' => __('Choose whether status you wish after checkout.', $this->domain),
-                    'default'     => 'wc-completed',
-                    'desc_tip'    => true,
-                    'options'     => wc_get_order_statuses()
-                ),
-                'description' => array(
-                    'title'       => __('Description', $this->domain),
-                    'type'        => 'textarea',
-                    'description' => __('Payment method description that the customer will see on your checkout.', $this->domain),
-                    'default'     => __('Pay by sending to Mpesa mobile number', $this->domain),
-                    'desc_tip'    => true,
-                ),
-                'mpesa_name' => array(
-                    'title'       => __('Mpesa Recipient Name', $this->domain),
-                    'type'        => 'text',
-                    'description' => __('Payment name that the customer will see on the mobile message to confirm.', $this->domain),
-                    //'default'     => __('Example: Joe Njenga', $this->domain),
-                    'desc_tip'    => true,
-                ),
+                // Customer Emails
+                add_action('woocommerce_email_before_order_table', array($this, 'email_instructions'), 10, 3);
+            }
 
-                'instructions' => array(
-                    'title'       => __('Instructions', $this->domain),
-                    'type'        => 'textarea',
-                    'description' => __('Instructions that will be added to the thank you page and emails.', $this->domain),
-                    'default'     => '',
-                    'desc_tip'    => true,
-                ),
-            );
-        }
+            /**
+             * Initialise Gateway Settings Form Fields.
+             */
+            public function init_form_fields()
+            {
 
-        /**
-         * Output for the order received page.
-         */
-        public function thankyou_page()
-        {
-            if ($this->instructions)
-                echo wpautop(wptexturize($this->instructions));
-        }
+                $this->form_fields = array(
+                    'enabled' => array(
+                        'title'   => __('Enable or Disable', $this->domain),
+                        'type'    => 'checkbox',
+                        'label'   => __('Enable Send Mpesa Payment', $this->domain),
+                        'default' => 'yes'
+                    ),
+                    'title' => array(
+                        'title'       => __('Payment Title', $this->domain),
+                        'type'        => 'text',
+                        'description' => __('This controls the title which the user sees during checkout.', $this->domain),
+                        'default'     => __('Send to Mpesa', $this->domain),
+                        'desc_tip'    => true,
+                    ),
+                    'order_status' => array(
+                        'title'       => __('Order Status', $this->domain),
+                        'type'        => 'select',
+                        'class'       => 'wc-enhanced-select',
+                        'description' => __('Choose whether status you wish after checkout.', $this->domain),
+                        'default'     => 'wc-completed',
+                        'desc_tip'    => true,
+                        'options'     => wc_get_order_statuses()
+                    ),
+                    'description' => array(
+                        'title'       => __('Description', $this->domain),
+                        'type'        => 'textarea',
+                        'description' => __('Payment method description that the customer will see on your checkout.', $this->domain),
+                        'default'     => __('Pay by sending to Mpesa mobile number', $this->domain),
+                        'desc_tip'    => true,
+                    ),
+                    'mpesa_name' => array(
+                        'title'       => __('Mpesa Recipient Name', $this->domain),
+                        'type'        => 'text',
+                        'description' => __('Payment name that the customer will see on the mobile message to confirm.', $this->domain),
+                        //'default'     => __('Example: Joe Njenga', $this->domain),
+                        'desc_tip'    => true,
+                    ),
 
-        /**
-         * Add content to the WC emails.
-         *
-         * @access public
-         * @param WC_Order $order
-         * @param bool $sent_to_admin
-         * @param bool $plain_text
-         */
-        public function email_instructions($order, $sent_to_admin, $plain_text = false)
-        {
-            if ($this->instructions && !$sent_to_admin && 'send_to_mpesa_' === $order->payment_method && $order->has_status('on-hold')) {
-                echo wpautop(wptexturize($this->instructions)) . PHP_EOL;
+                    'instructions' => array(
+                        'title'       => __('Instructions', $this->domain),
+                        'type'        => 'textarea',
+                        'description' => __('Instructions that will be added to the thank you page and emails.', $this->domain),
+                        'default'     => '',
+                        'desc_tip'    => true,
+                    ),
+               
+                );
+            
+            }
+
+            /**
+             * Output for the order received page.
+             */
+            public function thankyou_page()
+            {
+                if ($this->instructions)
+                    echo wpautop(wptexturize($this->instructions));
+            }
+
+            /**
+             * Add content to the WC emails.
+             *
+             * @access public
+             * @param WC_Order $order
+             * @param bool $sent_to_admin
+             * @param bool $plain_text
+             */
+            public function email_instructions($order, $sent_to_admin, $plain_text = false)
+            {
+                if ($this->instructions && !$sent_to_admin && 'send_to_mpesa_' === $order->payment_method && $order->has_status('on-hold')) {
+                    echo wpautop(wptexturize($this->instructions)) . PHP_EOL;
+                }
+            }
+
+            public function payment_fields()
+            {
+                 include __DIR__ . '/views/frontend.php';       
+            }
+
+            /**
+             * Process the payment and return the result.
+             *
+             * @param int $order_id
+             * @return array
+             */
+            public function process_payment($order_id)
+            {
+
+                $order = wc_get_order($order_id);
+
+                $status = 'wc-' === substr($this->order_status, 0, 3) ? substr($this->order_status, 3) : $this->order_status;
+
+                // Set order status
+                $order->update_status($status, __('Checkout with Mpesa payment. ', $this->domain));
+
+                // Reduce stock levels
+                $order->reduce_order_stock();
+
+                // Remove cart
+                WC()->cart->empty_cart();
+
+                // Return thankyou redirect
+                return array(
+                    'result'    => 'success',
+                    'redirect'  => $this->get_return_url($order)
+                );
             }
         }
 
-        public function payment_fields()
-        {
-          include __DIR__ . '/views/frontend.php';
-        }
+    }else{
 
-        /**
-         * Process the payment and return the result.
-         *
-         * @param int $order_id
-         * @return array
-         */
-        public function process_payment($order_id)
-        {
+        // If woocommerce is not active show the admin notice
 
-            $order = wc_get_order($order_id);
-
-            $status = 'wc-' === substr($this->order_status, 0, 3) ? substr($this->order_status, 3) : $this->order_status;
-
-            // Set order status
-            $order->update_status($status, __('Checkout with Mpesa payment. ', $this->domain));
-
-            // Reduce stock levels
-            $order->reduce_order_stock();
-
-            // Remove cart
-            WC()->cart->empty_cart();
-
-            // Return thankyou redirect
-            return array(
-                'result'    => 'success',
-                'redirect'  => $this->get_return_url($order)
-            );
-        }
+        add_action( 'admin_notices', 'woo_send_mpesa_payment_install_admin_notice' );
+   
     }
+
 }
 
 /**
- * Include other helper functions 
+ * Admin notice for WooCommerce activation requirement 
+ */ 
+ 
+function woo_send_mpesa_payment_install_admin_notice(){
+	 
+    echo '<div class="notice notice-error">' ."<br>";
+    echo  '<p>'.  _e( ' <strong> WooCommerce Send to Mpesa Payment Gateway</strong> plugin requires active WooCommerce Installation!', 'send-to-mpesa-payment' ).'</p>';
+    echo '</div>';
+   
+}
+
+/**
+ *  Include the helper functions 
  */
 
 require plugin_dir_path(__FILE__) . 'inc/functions.php';
+
+
+
